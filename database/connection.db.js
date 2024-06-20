7f84943969e84e38f41135079d2beeca82773b06
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2024-06-19 23:53:24 -0500

    Setting up Postgres database

---
+++ database/connection.db.js
@@ -0,0 +1,9 @@
+import pg from 'pg';
+
+const { Pool } = pg;
+const connectionString = process.env.DATABASE_URL;
+
+export const db = new Pool({
+    allowExitOnIdle: true,
+    connectionString,
+});
